73373506a97ac2c6a0977c32da5823d774675e0f
Prince Narteh <prinarteh@gmail.com>
2020-04-29 08:40:35 +0000

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app/layout/App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+const rootEl = document.getElementById("root");
+
+let render = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootEl
+  );
+};
+
+if (module.hot) {
+  module.hot.accept("./app/layout/App", () => setTimeout(render));
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
